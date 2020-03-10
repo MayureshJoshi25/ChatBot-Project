3c7ddcfa8c9fbd7dedd3c2839e13cab52eea23db
rahulkanade20 <60191253+rahulkanade20@users.noreply.github.com>
2020-03-10 20:07:17 +0530

    version before midsem review

--- vitbot.py
+++ vitbot.py
@@ -7,8 +7,8 @@ from chatterbot.trainers import ChatterBotCorpusTrainer
 #You can provide read_only=True if you want to disable the bot’s ability to learn after the training (i.e. from actual conversations).
 #logic_adapters is the list of adapters used to train the bot.
 my_bot = ChatBot(name='PyBot', read_only=False,
-                 logic_adapters=['chatterbot.logic.MathematicalEvaluation',
-                                 'chatterbot.logic.BestMatch'])
+                 logic_adapters=['chatterbot.logic.BestMatch',
+                                 'chatterbot.logic.MathematicalEvaluation'])
 
 #We are specifying the list of responses in the below part.
 small_talk = ['hi there!',
@@ -41,46 +41,53 @@ small_talk = ['hi there!',
               'information on college?',
               'Very good college']
 
+entity_placements = [
+                        'placements',
+                         '''In the past 9 years, around 111 companies have visited Vit every year,
+                            The average salary package for the past eleven years is rs 4.59 lakhs pr annum'''
+
+                        'Address',
+                        '''Vishwakarma Institute of Technology666, 
+                        Upper Indiranagar, Bibwewadi,Pune, Maharashtra, INDIA - 411 037.'''
+
+                    ]              
+
 inst_talk = [
                 'History of Vit',
-                '''Vishwakarma Institute of Technology, Pune, '
-                   a highly commendable private institute, occupies a place of pride amongst the premier technical institutes of the western region of India. 
-                   Established in the year 1983, financed and run by the Bansilal Ramnath Agrawal Charitable Trust, Pune. 
-                   It is affiliated to the University of Pune . 
-                   Within three decades, the institute marched towards the pinnacle of glory through its remarkable achievements and laurels in the field of engineering education of high calibre. 
-                   According to Indian history, "Vishwakarma" is a divine architect-engineer. 
-                   It is believed that He fashioned this world with His rare engineering skills. 
-                   The Trust adopted this name with a vision to develop engineers of high calibre, 
-                   who could take up challenges of any type of engineering job and become successful in the chosen career.''',
-                'You dont teach me',
-                'Yes sir',   
-                'Address of vit',
+                '''Vishwakarma Institute of Technology, Pune,
+                   a highly commendable private institute, occupies a place of pride amongst the premier technical institutes of the western region of India.''',
+                'Where is vit college located',
                 '''Vishwakarma Institute of Technology666, 
-                   Upper Indiranagar, Bibwewadi,Pune, Maharashtra, INDIA - 411 037.''',
+                  Upper Indiranagar, Bibwewadi,Pune, Maharashtra, INDIA - 411 037.''',
                 'Contact Details of vit',
                 'Contact Info : +91-20-2428 3001',
                 'Which courses are offered in Vit?',
                 '''Courses offered in vit are: 
-                   Undergrad BTech in chemical , computer , ENTC  , Industrial and Production,
+                  Undergrad BTech in chemical , computer , ENTC  , Industrial and Production,
                   Instrumentation , IT , mechanical. 
                   Postgrad courses : MCA and Mtech 
                   Phd Programs.''',
                 'Fees for BTech courses',
                 'The fee for BTech courses is around 1,80,000',
-                'Placements in Vit',
-                '''In the past 9 years, around 111 companies have visited Vit every year
-                   The average salary package for the past eleven years is rs 4.59 lakhs pr annum'''
+                'What are the placements rates in Vit',
+                '''In the past 9 years, around 111 companies have visited Vit every year,
+                  The average salary package for the past eleven years is rs 4.59 lakhs pr annum''',
                 'I dont care',
                 'Ok'   
                 ]
 
+                
+
 #We can create and train the bot by creating an instance of ListTrainer and supplying it with the lists of strings:
 list_trainer = ListTrainer(my_bot)
-for item in (small_talk, inst_talk):
+for item in (small_talk,entity_placements,inst_talk):
     list_trainer.train(item)
 trainer = ChatterBotCorpusTrainer(my_bot)
-trainer.train('chatterbot.corpus.english')    
+trainer.train('chatterbot.corpus.english.greetings')    
 
 #You have to use get_response method to input things. Things inside the method are customizable.  
 print(my_bot.get_response(input("Hello,how can I help you?")))
+for i in range(10):
+   print(my_bot.get_response(input()))
+   print(i)
 #print(my_bot.get_response())
