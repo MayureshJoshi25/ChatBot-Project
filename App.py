2c414a14f3f5a7e21d56f42134fd0de1594e067f
rahulkanade20 <60191253+rahulkanade20@users.noreply.github.com>
2020-03-16 19:59:29 +0530

    fixed the index html issue

--- App.py
+++ App.py
@@ -2,19 +2,21 @@ from flask import Flask, render_template, request
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 
-app = (__name__)
+app = Flask(__name__)
 english_bot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
 trainer = ChatterBotCorpusTrainer(english_bot)
 trainer.train("chatterbot.corpus.english")
  
-@app.route("/")
-def home():
-    return render_template("index.html")
-    
-@app.route("/get")
+
+@app.route('/startconversation',methods=['GET'])
 def get_bot_response():
     userText = request.args.get('msg')
     return str(english_bot.get_response(userText))
+
+@app.route("/")
+def home():
+    #return "<h1>Welcome to vit</h1>"
+    return render_template("index.html")    
  
  
 if __name__ == "__main__":
