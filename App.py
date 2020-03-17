e2d8a17622d35feb84d7861cd816baf493be854d
rahulkanade20 <60191253+rahulkanade20@users.noreply.github.com>
2020-03-17 10:34:21 +0530

    functional code

--- App.py
+++ App.py
@@ -8,9 +8,10 @@ trainer = ChatterBotCorpusTrainer(english_bot)
 trainer.train("chatterbot.corpus.english")
  
 
-@app.route('/startconversation',methods=['GET'])
+@app.route('/get',methods=['GET'])
 def get_bot_response():
     userText = request.args.get('msg')
+    print("msg from user is : "+ userText)
     return str(english_bot.get_response(userText))
 
 @app.route("/")
@@ -21,3 +22,5 @@ def home():
  
 if __name__ == "__main__":
     app.run()
+
+    
